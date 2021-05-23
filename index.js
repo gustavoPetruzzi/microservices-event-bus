247e80cc3afa63ec4ddedf06d9f980d0d46a0ab4
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2021-05-23 14:16:16 -0300

    handle new events

--- index.js
+++ index.js
@@ -6,15 +6,35 @@ const app = express();
 
 app.use(bodyparser.json());
 
+const events = [];
 app.post('/events', (req, res) => {
   const event = req.body;
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log('4000 ', err);
+  });
+
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log('4001 ', err);
+  });
+
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log('4002 ', err);
+  });
+
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log('4003 ', err);
+  });
 
   res.send({ status: 'OK' });
 });
 
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
 app.listen(4005, () => {
-  console.log('listening on 4005');
+  console.log('Listening on 4005');
 });
